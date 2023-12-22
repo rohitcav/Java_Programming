977dce68d6dd1ce32524b01cd664bb043488fbb4
krorohi <rohit.av.kumar@oracle.com>
2023-12-22 11:40:55 +0530

    new pallindrom program

--- PallindromeNumber.java
+++ PallindromeNumber.java
@@ -10,7 +10,30 @@ public class PallindromeNumber {
     }
 
     public static boolean isPalindrome(int number)
-    {
+    { public static void main(String[] args)
+        {
+            int number= 22122;
+            if(isPalindrome(number))
+            {
+                System.out.println("The number: "+ number +" is a Palindrome number" );
+            }
+            else System.out.println("The number: "+ number +" is not a Palindrome number" );
+        }
+
+        public static boolean isPalindrome(int number)
+        {
+            int reverse_number=0;
+            int orig_number = number;
+            while(number!=0)
+            {
+                int remainder= number%10;
+                reverse_number= reverse_number*10 + remainder;
+                number= number/10;
+
+            }
+            System.out.println(reverse_number);
+            return orig_number == reverse_number;
+        }
         int reverse_number=0;
         int orig_number = number;
         while(number!=0)
