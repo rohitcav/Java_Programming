1a9ec86a75fabb7de387e402c1566f892126d4c4
krorohi <rohit.av.kumar@oracle.com>
2024-01-09 13:25:40 +0530

    Java Program for pallindrome number.

---
+++ megaByteConverter.java
@@ -0,0 +1,10 @@
+public class megaByteConverter {
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(2500);
+    }
+    public static void printMegaBytesAndKiloBytes(int kilobyte){
+        int MB= kilobyte/1024;
+        int KB= kilobyte%1024;
+        System.out.println(kilobyte + " KB = "+ MB + " MB and "+ KB + " KB");
+    }
+}
---
+++ speedConverter.java
@@ -0,0 +1,17 @@
+public class speedConverter {
+    public static void main(String[] args)
+    {
+       double MPH= toMilesPerHour(2.5);
+       System.out.println("2.5 KMPH = " + MPH + "MPH");
+
+    }
+    public static long toMilesPerHour(double kilometersPerHour) {
+
+        if (kilometersPerHour < 0) {
+            return -1;
+        } else if (kilometersPerHour > 0) {
+            return (long)(kilometersPerHour * 1.609);
+        }
+        return 0;
+    }
+}
